6cdaefc07741c4c0022f410fcc5f3f40fd069db5
John Cuenca <johnchristiensen.cuenca.cics@ust.edu.ph>
2023-08-23 07:24:08 +0800

    navbar

---
+++ app/src/main/java/com/example/aktibo/ExerciseActivity.kt
@@ -0,0 +1,39 @@
+package com.example.aktibo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+
+        // Initialize and assign variable
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Set Home selected
+        bottomNavigationView.selectedItemId = R.id.exercise
+
+        // Perform item selected listener
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.exercise -> return@OnNavigationItemSelectedListener true
+                R.id.home -> {
+                    startActivity(Intent(applicationContext, HomeActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.food -> {
+                    startActivity(Intent(applicationContext, FoodActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+    }
+}
--- app/src/main/java/com/example/aktibo/HomeActivity.kt
+++ app/src/main/java/com/example/aktibo/HomeActivity.kt
@@ -1,13 +1,15 @@
 package com.example.aktibo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.ComponentActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
 
     public override fun onStart() {
@@ -49,6 +51,32 @@ class HomeActivity : AppCompatActivity() {
         logoutButton.setOnClickListener {
             auth.signOut();
         }
+
+        // Initialize and assign variable
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Set Home selected
+        bottomNavigationView.selectedItemId = R.id.home
+
+        // Perform item selected listener
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> return@OnNavigationItemSelectedListener true
+                R.id.food -> {
+                    startActivity(Intent(applicationContext, FoodActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.exercise -> {
+                    startActivity(Intent(applicationContext, ExerciseActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
 
     }
 }
