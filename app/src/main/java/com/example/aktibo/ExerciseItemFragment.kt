6de8bfe3097f80ce6ecf44c2b9aff1ef640cc4d4
John Cuenca <johnchristiensen.cuenca.cics@ust.edu.ph>
2023-10-25 06:34:15 +0800

    exercise items

--- app/src/main/java/com/example/aktibo/ExerciseItemFragment.kt
+++ app/src/main/java/com/example/aktibo/ExerciseItemFragment.kt
@@ -1,20 +1,37 @@
 package com.example.aktibo
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.VideoView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.aktibo.LoginActivity.Companion.TAG
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ExerciseItemFragment : Fragment() {
+    private lateinit var exerciseID: String
+    private lateinit var videoView: VideoView
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val bundle = arguments
+        if (bundle != null) {
+            val data = bundle.getString("exerciseID")
+            if (data != null) {
+                exerciseID = data
+            }
+        }
     }
 
     override fun onCreateView(
@@ -24,38 +41,100 @@ class ExerciseItemFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_exercise_item, container, false)
 
+        val db = Firebase.firestore
+        val docRef = db.collection("exercises").document(exerciseID)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
 
-        // Text Instruction
-        val items = arrayOf(
-            "Start on your hands and toes, with your hands slightly wider than shoulder-width apart.",
-            "Keep your body straight from head to toes, like a plank.",
-            "Bend your arms and lower your body until your chest touches the ground or goes as low as you can comfortably.",
-            "Push through your hands and straighten your arms to raise your body back up.",
-            "Keep your body straight throughout the movement.",
-            "Repeat for the desired number of repetitions, maintaining good form.",
-            "Repeat for the desired number of repetitions, maintaining good form.",
-            "Repeat for the desired number of repetitions, maintaining good form.",
-            "Repeat for the desired number of repetitions, maintaining good form.",
-            "Repeat for the desired number of repetitions, maintaining good form.")
+                    val textViewExerciseName = view.findViewById<TextView>(R.id.textViewExerciseName)
+                    textViewExerciseName.text = document.data?.get("name").toString()
 
-        val layoutInstructions: LinearLayout = view.findViewById(R.id.layoutInstructions)
+                    val reps_duration = document.data?.get("reps_duration").toString()
+                    val sets = document.data?.get("sets").toString()
+                    val textViewRepSets = view.findViewById<TextView>(R.id.textViewRepSets)
+                    if (containsOnlyDigits(reps_duration)){
+                        textViewRepSets.text = "${reps_duration} repetitions (${sets} set/s)"
+                    } else {
+                        textViewRepSets.text = "${reps_duration} (${sets} set/s)"
+                    }
+                    val videoUrl = document.data?.get("video").toString()
+                    videoView = view.findViewById(R.id.videoView)
+                    playVideo(videoUrl)
 
-        for ((index, item) in items.withIndex()) {
-            val customItem = inflater.inflate(R.layout.ordered_list_item, null) as LinearLayout
+                    // Text Instruction
+                    val items: ArrayList<String> = document.data?.get("instructions") as ArrayList<String>
+//                    val items = arrayOf(
+//                        "Start on your hands and toes, with your hands slightly wider than shoulder-width apart.",
+//                        "Keep your body straight from head to toes, like a plank.",
+//                        "Bend your arms and lower your body until your chest touches the ground or goes as low as you can comfortably.",
+//                        "Push through your hands and straighten your arms to raise your body back up.",
+//                        "Keep your body straight throughout the movement.",
+//                        "Repeat for the desired number of repetitions, maintaining good form.",
+//                        "Repeat for the desired number of repetitions, maintaining good form.",
+//                        "Repeat for the desired number of repetitions, maintaining good form.",
+//                        "Repeat for the desired number of repetitions, maintaining good form.",
+//                        "Repeat for the desired number of repetitions, maintaining good form.")
 
-            val textViewNumber = customItem.findViewById<TextView>(R.id.textViewNumber)
-            val listNum = index+1
-            val textViewNumberText = "$listNum."
-            textViewNumber.text = textViewNumberText
+                    val layoutInstructions: LinearLayout = view.findViewById(R.id.layoutInstructions)
 
-            val textViewStep = customItem.findViewById<TextView>(R.id.textViewStep)
-            textViewStep.text = item
+                    for ((index, item) in items.withIndex()) {
+                        val customItem = inflater.inflate(R.layout.ordered_list_item, null) as LinearLayout
 
-            layoutInstructions.addView(customItem)
-        }
+                        val textViewNumber = customItem.findViewById<TextView>(R.id.textViewNumber)
+                        val listNum = index+1
+                        val textViewNumberText = "$listNum."
+                        textViewNumber.text = textViewNumberText
+
+                        val textViewStep = customItem.findViewById<TextView>(R.id.textViewStep)
+                        textViewStep.text = item
+
+                        layoutInstructions.addView(customItem)
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+
 
 
         return view
+    }
+
+    private fun playVideo(videoUrl: String) {
+        // Set the media controller (optional)
+        val mediaController = android.widget.MediaController(requireContext())
+        videoView.setMediaController(mediaController)
+
+        // Set the video URI using the provided URL
+        val videoUri = Uri.parse(videoUrl)
+        videoView.setVideoURI(videoUri)
+
+        // Start video playback
+        videoView.start()
+
+        // Initialize the MediaPlayer for additional control (optional)
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource(requireContext(), videoUri)
+        mediaPlayer.prepare()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Release resources when the fragment is paused
+        videoView.stopPlayback()
+        mediaPlayer.release()
+    }
+
+    fun containsOnlyDigits(input: String): Boolean {
+        for (char in input) {
+            if (!char.isDigit()) {
+                return false
+            }
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/example/aktibo/ExerciseListFragment.kt
+++ app/src/main/java/com/example/aktibo/ExerciseListFragment.kt
@@ -5,19 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ExerciseListFragment : Fragment() {
+    private lateinit var intensity: String
+    private lateinit var region: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val bundle = arguments
         if (bundle != null) {
-            val intensity = bundle.getString("intensity")
-            val region = bundle.getString("region")
+            intensity = bundle.getString("intensity").toString()
+            region = bundle.getString("region").toString()
 
-            println("$intensity, $region")
+            // println("$intensity, $region")
         }
     }
 
@@ -26,7 +33,131 @@ class ExerciseListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_exercise_list, container, false)
+
+        var query_region = ""
+        var query_intensity = ""
+
+        val textViewTitle = view.findViewById<TextView>(R.id.textViewTitle)
+        when (region) {
+            "upper" -> {
+                textViewTitle.text = "Upper Body Exercises"
+                query_region = "Upper Body"
+            }
+            "lower" -> {
+                textViewTitle.text = "Lower Body Exercises"
+                query_region = "Lower Body"
+            }
+            "whole" ->{
+                textViewTitle.text = "Whole Body Exercises"
+                query_region = "Whole Body"
+            }
+        }
+
+        when (intensity) {
+            "light" -> {
+                query_intensity = "Easy"
+            }
+            "moderate" -> {
+                query_intensity = "Intermediate"
+            }
+            "vigorous" ->{
+                query_intensity = "Advanced"
+            }
+        }
+
+        val linearLayout = view.findViewById<LinearLayout>(R.id.scrollContainer)
+
+        val db = Firebase.firestore
+        db.collection("exercises")
+            .whereEqualTo("category", query_region)
+            .whereEqualTo("intensity", query_intensity)
+            .get()
+            .addOnSuccessListener { queryDocumentSnapshots ->
+                for (data in queryDocumentSnapshots){
+                    val id = data.id
+                    val name = data.getString("name").toString()
+                    val reps_duration = data["reps_duration"].toString()
+                    val sets = data.getDouble("sets")?.toInt().toString()
+                    val est_time = data["est_time"].toString()
+
+                    val tags: ArrayList<String> = data.data.get("tags") as ArrayList<String>
+
+                    var tagsString = ""
+                    for ((index, tag) in tags.withIndex()) {
+                        if (index == tags.size - 1) {
+                            tagsString += "${tag}"
+                        } else {
+                            tagsString += "${tag}, "
+                        }
+                    }
+
+                    val itemLayout = inflater.inflate(R.layout.exercise_item, null)
+
+                    val marginLayoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        resources.getDimensionPixelSize(R.dimen.exer_item_height)
+                    )
+                    marginLayoutParams.setMargins(
+                        0,
+                        0,
+                        0,
+                        resources.getDimensionPixelSize(R.dimen.bottom_margin)
+                    ) // Adjust the margin as needed
+                    itemLayout.layoutParams = marginLayoutParams
+
+                    val nameView = itemLayout.findViewById<TextView>(R.id.exerciseName)
+                    nameView.text = name
+
+                    val exerciseInfo = itemLayout.findViewById<TextView>(R.id.exerciseInfo)
+                    if (containsOnlyDigits(reps_duration)){
+                        exerciseInfo.text = "${reps_duration} reps - ${sets} sets\nEst.: ${est_time}"
+                    } else {
+                        exerciseInfo.text = "${reps_duration}\n${sets} sets\nEst.: ${est_time}"
+                    }
+
+                    val exerciseTags = itemLayout.findViewById<TextView>(R.id.exerciseTags)
+                    exerciseTags.text = tagsString
+
+                    val layout = itemLayout.findViewById<LinearLayout>(R.id.layout)
+                    layout.setOnClickListener{
+                        replaceFragmentWithAnimWithData(ExerciseItemFragment(), id)
+                    }
+
+                    linearLayout.addView(itemLayout)
+                }
+            }
+
+        return view
+    }
+
+    private fun replaceFragmentWithAnimWithData(fragment: Fragment, exerciseID: String) {
+        val bundle = Bundle()
+        bundle.putString("exerciseID", exerciseID)
+
+        val newFragment = fragment
+        newFragment.arguments = bundle
+
+        val fragmentManager = getParentFragmentManager()
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.setCustomAnimations(
+            R.anim.slide_in_right, // Enter animation
+            R.anim.slide_out_left, // Exit animation
+            R.anim.slide_in_left, // Pop enter animation (for back navigation)
+            R.anim.slide_out_right // Pop exit animation (for back navigation)
+        )
+        fragmentTransaction.replace(R.id.fragment_container, newFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    fun containsOnlyDigits(input: String): Boolean {
+        for (char in input) {
+            if (!char.isDigit()) {
+                return false
+            }
+        }
+        return true
     }
 
 }
